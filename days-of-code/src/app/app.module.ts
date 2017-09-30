2b1780e4958c3b3fae5db4279c2dc88cf4169130
felipejsm <felipejsm41@gmail.com>
2017-09-30 12:03:18 -0300

    Adding my pages in modules

--- days-of-code/src/app/app.module.ts
+++ days-of-code/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ChartsPage } from '../pages/charts/charts';
+import { ConfigurationsPage } from '../pages/configurations/configurations';
+import { MyChallengePage } from '../pages/my-challenge/my-challenge';
+import { NewChallengePage } from '../pages/new-challenge/new-challenge';
+import { AboutPage } from '../pages/about/about';
+import { HistoryPage } from '../pages/history/history';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +19,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ChartsPage,
+    ConfigurationsPage,
+    MyChallengePage,
+    NewChallengePage,
+    AboutPage,
+    HistoryPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +35,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ChartsPage,
+    ConfigurationsPage,
+    MyChallengePage,
+    NewChallengePage,
+    AboutPage,
+    HistoryPage
   ],
   providers: [
     StatusBar,
